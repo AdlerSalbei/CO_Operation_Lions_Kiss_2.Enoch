15ea671a6869c67548ab4b4bb431ab2f8f765188
Salbei <salbei@gruppe-adler.de>
2019-10-02 10:56:09 +0200

    added cbrn

--- cfgEventHandlers.hpp
+++ cfgEventHandlers.hpp
@@ -13,3 +13,12 @@ class Extended_Local_EventHandlers {
         };
     };
 };
+class Extended_PreInit_EventHandlers {
+    class diwako_cbrn_preInit {
+        init = "[] call compile preProcessFileLineNumbers 'node_modules\diwako_cbrn\XEH_PreInit.sqf'";
+    };
+};
+
+class Extended_InitPost_EventHandlers {
+    #include "node_modules\diwako_cbrn\XEH_InitPost_eh.hpp"
+};
