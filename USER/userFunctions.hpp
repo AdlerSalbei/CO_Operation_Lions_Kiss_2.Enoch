901c83df2ace5f4ee48a09bc96891704ff09562e
Salbei <salbei@gruppe-adler.de>
2019-10-05 02:00:08 +0200

    added Odys Briefing

--- USER/userFunctions.hpp
+++ USER/userFunctions.hpp
@@ -6,6 +6,7 @@
 class GRAD_USER {
     class common {
         file = "USER\functions";
+        class addPanelActions {};
         class chairCircle {};
         class closingCredits {};
         class createVehicle {};
